8ab5ec764c8b5d5c39b6d65a53c90d4f44cb131d
Stafford Williams <stafford.williams@gmail.com>
2025-11-09 12:56:05 +1100

    bump version

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ docker buildx create --use
 
 # Build for both architectures and push to registry
 docker buildx build --platform linux/amd64,linux/arm64 \
-  -t staff0rd/image-generator:4.1 \
+  -t staff0rd/image-generator:4.2 \
   --push \
   .
